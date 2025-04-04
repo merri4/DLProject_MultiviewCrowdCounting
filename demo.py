1a382a66daf2f4e57b2fde7c634e33f624460646
merri4 <kjhwang97@gmail.com>
2025-04-04 20:05:25 +0900

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+import gradio as gr
+from model import *
+
+
+if __name__ == "__main__" :
+    
+    pass
---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import transformers
+
+
+class MultiviewFusionModel(nn.Module) :
+    def __init__(self) :
+        super().__init__()
+    
+    def fowrard(self, x1, x2) :
+        pass
+
+
+if __name__ == "__main__" :
+    pass
