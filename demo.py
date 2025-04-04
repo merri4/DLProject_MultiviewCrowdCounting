4b0ad918aa0c47050f78f5da37139e820a4a913f
merri4 <kjhwang97@gmail.com>
2025-04-04 20:15:38 +0900

    feat: demo 샘플 추가

--- demo.py
+++ demo.py
@@ -1,7 +1,17 @@
 import gradio as gr
 from model import *
 
+def predict(image1, image2):
+    density_map, count = MultiviewFusionModel(image1, image2)
+    return density_map, count
 
 if __name__ == "__main__" :
     
-    pass
+    demo = gr.Interface(
+        fn=predict,
+        inputs=[gr.Image(label="View 1"), gr.Image(label="View 2")],
+        outputs=[gr.Image(label="Density Map"), gr.Number(label="Count")],
+        title="Multi-View Crowd Counter"
+    )
+    
+    demo.launch()
