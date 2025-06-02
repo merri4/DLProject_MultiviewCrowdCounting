9aa51230a32a0cf36394b405f1abf28b0418d7d8
merri4 <129365028+merri4@users.noreply.github.com>
2025-06-02 13:22:37 +0900

    Feat-Residual connection and confidence output (#3)
    * add: residual block, confidence head
    
    * fix : inference

--- demo.py
+++ demo.py
@@ -25,17 +25,29 @@ def load_checkpoint(filename, model, optimizer=None):
     return epoch, loss
 
 # 모델 불러와두기
-MODEL_PATH = "./output/epoch_1.pth"
+MODEL_PATH = "./output_rescon/epoch_3.pth"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = MultiviewFusionModel().to(DEVICE)
 load_checkpoint(MODEL_PATH, model)
 
 
-def coords_to_density_map(coords, H, W, sigma=6) :
+def coords_to_density_map(output, H, W, sigma=6, threshold=0.5) :
+
+    output = output.detach().cpu().numpy()
+
+    conf = output[:, :, 2].squeeze(0)       # [N, 1]
+    coords = output[:, :, :2].squeeze(0)    # [N, 2]
+
+    true_coords = []
+    for i,c in enumerate(coords) :
+        if conf[i] > threshold :
+            true_coords.append(c)
+    
+    count = len(true_coords)
 
     density = np.zeros((H, W), dtype=np.float32)
     
-    for x, y in coords :
+    for x, y in true_coords :
         x_pix = int(np.clip(x, 0, 1) * (W - 1))
         y_pix = int(np.clip(y, 0, 1) * (H - 1))
         if 0 <= x_pix < W and 0 <= y_pix < H:
@@ -44,16 +56,13 @@ def coords_to_density_map(coords, H, W, sigma=6) :
     density = cv2.GaussianBlur(density, (15, 15), sigma)
     
     if density.sum() > 0:
-        density = density * (len(coords) / density.sum())
+        density = density * (len(true_coords) / density.sum())
     
-    return density
+    return density, count
 
-def predict(image1, image2) :
+def predict(image1, image2, threshold) :
     
     # 이미지 전처리
-    print(image1.shape)
-    print(image2.shape)
-
     left_img = Image.fromarray(image1).convert('RGB').resize((W, H))
     right_img = Image.fromarray(image2).convert('RGB').resize((W, H))
     
@@ -61,18 +70,11 @@ def predict(image1, image2) :
     left_img = to_tensor(left_img).unsqueeze(0).to(DEVICE)
     right_img = to_tensor(right_img).unsqueeze(0).to(DEVICE)
 
-    print(left_img.shape)
-    print(right_img.shape)
-
     # 모델 feeding
     with torch.no_grad() :
-        pred_coords = model(left_img, right_img)
+        output = model(left_img, right_img)
     
-    pred = pred_coords[0, :MAX_NUMBER].cpu().numpy()
-    pred_density = coords_to_density_map(pred, H, W, sigma=6)
-    count = len(pred)
-
-    print(count)
+    pred_density, count = coords_to_density_map(output, H, W, sigma=6, threshold=threshold)
 
     density_map = plt.figure()
     ax = density_map.add_subplot(1,1,1)
@@ -91,7 +93,7 @@ if __name__ == "__main__" :
     
     demo = gr.Interface(
         fn=predict,
-        inputs=[gr.Image(label="View 1"), gr.Image(label="View 2")],
+        inputs=[gr.Image(label="View 1"), gr.Image(label="View 2"), gr.Slider(label="Confidence", minimum=0, maximum=1.0, value=0.5, step=0.05)],
         outputs=[gr.Plot(label="Density Map"), gr.Number(label="Count")],
         title="Multi-View Crowd Counter",
         description="This demo estimates the crowd count from two different views of the same scene."
